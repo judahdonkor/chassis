491c2e3d6d6a71940ddc2152a50a5991b5ff86fe
Judah Donkor <judahdonkor@gmail.com>
2020-10-11 08:04:12 +0000

    Add constructor

--- src/main/java/org/judahdonkor/chassis/SuggestedInputOption.java
+++ src/main/java/org/judahdonkor/chassis/SuggestedInputOption.java
@@ -1,10 +1,14 @@
 package org.judahdonkor.chassis;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SuggestedInputOption {
     private String value, display;
 }
